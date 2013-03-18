ed82085251a2cfeef543c09397f600fafccb247c
Mike Berlin <mikeberlin@me.com>
2013-03-18 18:52:37 -0500

    Initial Commit

---
+++ ConcertFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConcertFinder
+//
+//  Created by Mike Berlin on 3/18/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ ConcertFinder/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  ConcertFinder
+//
+//  Created by Mike Berlin on 3/18/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
